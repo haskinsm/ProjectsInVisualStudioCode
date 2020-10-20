a2adfc2bed2e1d1ddb5f2e1726bdc2fef73e6ede
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-10-20 20:59:03 +0100

    Unit testing added for LCA Python version, both tests added so far pass

--- LCAncestor Python Version/test_LCA.py
+++ LCAncestor Python Version/test_LCA.py
@@ -4,28 +4,43 @@ import LCAncestor
 
 class test_LCA(unittest.TestCase):
 
-    BST = BinaryTree('d')
-
-    BST.root.left_Node = LCAncestor.Node('c')
-    BST.root.left_Node.Parent = BST.root
-    BST.root.right_Node = LCAncestor.Node('e')
-    BST.root.right_Node.Parent = BST.root
-
-    BST.root.left_Node.left_Node = LCAncestor.Node('a')
-    BST.root.left_Node.left_Node.Parent = BST.root.left_Node
-    BST.root.left_Node.right_Node = LCAncestor.Node('b')
-    BST.root.left_Node.right_Node.Parent = BST.root.left_Node
-
-    BST.root.right_Node.left_Node = LCAncestor.Node('f')
-    BST.root.right_Node.left_Node.Parent = BST.root.right_Node
-    BST.root.right_Node.right_Node = LCAncestor.Node('g')
-    BST.root.right_Node.right_Node.Parent = BST.root.right_Node
-
     def test_BST(self):
-        self.assertEqual(LCAncestor.BST.root.val, 'd', "Should be d")
+        BST = LCAncestor.BinaryTree('d')
+
+        BST.root.left_Node = LCAncestor.Node('c')
+        BST.root.left_Node.Parent = BST.root
+        BST.root.right_Node = LCAncestor.Node('e')
+        BST.root.right_Node.Parent = BST.root
+
+        BST.root.left_Node.left_Node = LCAncestor.Node('a')
+        BST.root.left_Node.left_Node.Parent = BST.root.left_Node
+        BST.root.left_Node.right_Node = LCAncestor.Node('b')
+        BST.root.left_Node.right_Node.Parent = BST.root.left_Node
+
+        BST.root.right_Node.left_Node = LCAncestor.Node('f')
+        BST.root.right_Node.left_Node.Parent = BST.root.right_Node
+        BST.root.right_Node.right_Node = LCAncestor.Node('g')
+        BST.root.right_Node.right_Node.Parent = BST.root.right_Node
+        self.assertEqual(BST.root.val, 'd', "Should be d")
 
     def test_LCA(self):
-        self.assertEqual(LCAncestor.lowest_common_ancestor(BST.root.right_Node.left_Node,BST.root.right_Node.right_Node), 'f', "Should be f")
+        BST = LCAncestor.BinaryTree('d')
+
+        BST.root.left_Node = LCAncestor.Node('c')
+        BST.root.left_Node.Parent = BST.root
+        BST.root.right_Node = LCAncestor.Node('e')
+        BST.root.right_Node.Parent = BST.root
+
+        BST.root.left_Node.left_Node = LCAncestor.Node('a')
+        BST.root.left_Node.left_Node.Parent = BST.root.left_Node
+        BST.root.left_Node.right_Node = LCAncestor.Node('b')
+        BST.root.left_Node.right_Node.Parent = BST.root.left_Node
+
+        BST.root.right_Node.left_Node = LCAncestor.Node('f')
+        BST.root.right_Node.left_Node.Parent = BST.root.right_Node
+        BST.root.right_Node.right_Node = LCAncestor.Node('g')
+        BST.root.right_Node.right_Node.Parent = BST.root.right_Node
+        self.assertEqual(LCAncestor.lowest_common_ancestor(BST.root.right_Node.left_Node,BST.root.right_Node.right_Node).val, 'e', "Should be e")
 
 if __name__ == '__main__':
     unittest.main()
