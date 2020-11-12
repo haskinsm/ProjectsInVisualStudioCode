ae572277afc8285b3a307886745a3fb6ed18eee6
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-11-12 15:42:16 +0000

    Nearly finished

--- Exercise02/src/Ex02.hs
+++ Exercise02/src/Ex02.hs
@@ -1,5 +1,7 @@
 {- butrfeld Andrew Butterfield -}
 module Ex02 where
+  --Also not sure if you wanted me to replace your name with mine up there, apologies if I have got this wrong
+import Data.List hiding (find)  --Imported as used concat 
 
 name, idno, username :: String
 name      =  "Michael Haskins"  -- replace with your name
@@ -57,13 +59,45 @@ v42 = Val 42 ; j42 = Just v42
   -- see test outcomes for the precise format of those messages
 
 eval :: EDict -> Expr -> Either String Double --Might need to change the last to Maybe Double, but returns string on occasion so think its ok as is
-eval d (Add x y) =
-eval d (Mul x y) =
-eval d (Sub x y) =
-eval d (Dvd x (val 0)) = Nothing --So as to prohibit deviding by 0
-eval d (Dvd x y) =
-eval d (var a) = find d a
+eval d (Var a) = find d a
+eval _ (Val a) = Right a --Presume do not need the dict as i think theres no need for it here
+
+eval d (Add x y) 
+   = case (eval d x, eval d y) of --Evaluates both expressions and pattern matches the case
+     (Right a, Right b) -> Right (a+b) --Add values
+     (Left a, Left b) -> Left(concat(a,b)) --Add/Concat Strings, might take this out 
+     (Right a, _)     -> Left "undefined var y"
+     (_, Right b)     -> Left "undefined var x"
+     _                -> Left "undefined"
+          
+
+eval d (Mul x y)
+   = case (eval d x, eval d y) of
+     (Right a, Right b) -> Right(a*b)
+     (Right a, _)       -> Left "undefined var y"
+     (_, Right a)       -> Left "undefined var x"
+     _                  -> Left "undefined"
+
+eval d (Sub x y)
+   = case (eval d x, eval d y) of 
+     (Right a, Right b) -> Right(a-b)
+     (Right a, _)       -> Left "undefined var y"
+     (_, Right a)       -> Left "undefined var x"
+     _                  -> Left "undefined"
+
+eval d (Dvd x (Val 0)) = Left "div by zero" --So as to prohibit deviding by 0
+eval d (Dvd x y) 
+    = case (eval d x, eval d y) of 
+      (Right a, Right b) -> Right(a/b)
+      (Right a, _)       -> Left "undefined var y"
+      (_, Right a)       -> Left "undefined var x"
+      _                  -> Left "undefined"
+
 eval d (Def a x y) 
+    = let z = eval d x --Evaluates expression x
+    in case (z) of 
+      (Right z) -> eval (define d a z) y --Adds the result of eval. expr x to the dict and uses newly created dict for expr b
+      _         -> Left "undefined"
 
 -- Part 1 : Expression Laws -- (15 test marks, worth 15 Exercise Marks) --------
 
@@ -71,7 +105,7 @@ eval d (Def a x y)
 
 There are many, many laws of algebra that apply to our expressions, e.g.,
 
-  x + y            =  y + z         Law 1
+  x + y            =  y + x         Law 1
   x + (y + z)      =  (x + y) + z   Law 2
   x - (y + z)      =  (x - y) - z   Law 3
   (x + y)*(x - y)  =  x*x - y*y     Law 4
@@ -89,13 +123,21 @@ There are many, many laws of algebra that apply to our expressions, e.g.,
 
 
 law1 :: Expr -> Maybe Expr
-law1 e = error "law1 NYI"
+law1 e = case e of 
+          (Add x y) -> Just (Add y x) --Use Just/Nothing now not Right/Left
+          _         -> Nothing
 
 law2 :: Expr -> Maybe Expr
-law2 e = error "law2 NYI"
+law2 e = case e of 
+          (Add x (Add y z)) -> Just (Add (Add x y) z)
+          _                 -> Nothing
 
 law3 :: Expr -> Maybe Expr
-law3 e = error "law3 NYI"
+law3 e = case e of 
+          (Sub x (Add y z)) -> Just(Sub(Sub x y) z)
+          _                 -> Nothing
 
-law4 :: Expr -> Maybe Expr
-law4 e = error "law4 NYI"
+law4 :: Expr -> Maybe Expr --Will need to write a helper function of some sorts to make sure x == z, y== q
+law4 e = case e of    
+          (Mul (Add x y) (Sub z q)) -> Just(Sub (Mul x z) (Mul y q)) --Note need x == z and y == q
+          _                         -> Nothing
