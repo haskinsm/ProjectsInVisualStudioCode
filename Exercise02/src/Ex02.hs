50fbe400d5d84a866ea0ed27f2b7903a2ed376e9
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-11-13 16:58:22 +0000

    Haskell Exercise02 All test passings

--- Exercise02/src/Ex02.hs
+++ Exercise02/src/Ex02.hs
@@ -1,7 +1,6 @@
 {- butrfeld Andrew Butterfield -}
 module Ex02 where
   --Also not sure if you wanted me to replace your name with mine up there, apologies if I have got this wrong
-import Data.List hiding (find)  --Imported as used concat 
 
 name, idno, username :: String
 name      =  "Michael Haskins"  -- replace with your name
@@ -29,7 +28,7 @@ data Expr
   | Mul Expr Expr
   | Sub Expr Expr
   | Dvd Expr Expr
-  | Var Id
+  | Var Id   
   | Def Id Expr Expr
   deriving (Eq, Show)
 
@@ -58,46 +57,45 @@ v42 = Val 42 ; j42 = Just v42
   -- (2) the expression contains a variable not in the dictionary.
   -- see test outcomes for the precise format of those messages
 
+--Var is of type String and Val of type Double
+
 eval :: EDict -> Expr -> Either String Double --Might need to change the last to Maybe Double, but returns string on occasion so think its ok as is
-eval d (Var a) = find d a
-eval _ (Val a) = Right a --Presume do not need the dict as i think theres no need for it here
+eval d (Var a) = find d a --Need Dict here
+eval _ (Val a) = Right a --Do not need the dict 
 
 eval d (Add x y) 
    = case (eval d x, eval d y) of --Evaluates both expressions and pattern matches the case
-     (Right a, Right b) -> Right (a+b) --Add values
-     (Left a, Left b) -> Left(concat(a,b)) --Add/Concat Strings, might take this out 
-     (Right a, _)     -> Left "undefined var y"
-     (_, Right b)     -> Left "undefined var x"
-     _                -> Left "undefined"
+     (Right a, Right b) -> Right (a+b) --Add values 
+     (_, Left b)  -> Left (b) --Need to have this so prints error message that may arrive from eval d y
+     (Left a, _)  -> Left (a)
           
-
 eval d (Mul x y)
    = case (eval d x, eval d y) of
      (Right a, Right b) -> Right(a*b)
-     (Right a, _)       -> Left "undefined var y"
-     (_, Right a)       -> Left "undefined var x"
-     _                  -> Left "undefined"
+     (_, Left b)  -> Left (b) --Need to have this so prints error message that may arrive from eval d y
+     (Left a, _)  -> Left (a)
 
 eval d (Sub x y)
    = case (eval d x, eval d y) of 
      (Right a, Right b) -> Right(a-b)
-     (Right a, _)       -> Left "undefined var y"
-     (_, Right a)       -> Left "undefined var x"
-     _                  -> Left "undefined"
+     (_, Left b)  -> Left (b) --Need to have this so prints error message that may arrive from eval d y
+     (Left a, _)  -> Left (a)
 
-eval d (Dvd x (Val 0)) = Left "div by zero" --So as to prohibit deviding by 0
+eval _ (Dvd x (Val 0)) = Left "div by zero" --So as to prohibit deviding by 0
 eval d (Dvd x y) 
     = case (eval d x, eval d y) of 
       (Right a, Right b) -> Right(a/b)
-      (Right a, _)       -> Left "undefined var y"
-      (_, Right a)       -> Left "undefined var x"
-      _                  -> Left "undefined"
+      (_, Left b)  -> Left (b) --Need to have this so prints error message that may arrive from eval d y
+      (Left a, _)  -> Left (a)
+     
 
 eval d (Def a x y) 
     = let z = eval d x --Evaluates expression x
     in case (z) of 
       (Right z) -> eval (define d a z) y --Adds the result of eval. expr x to the dict and uses newly created dict for expr b
-      _         -> Left "undefined"
+      (Left z)  -> Left (z) --Need to have this so prints error message that may arrive from eval d y
+      
+
 
 -- Part 1 : Expression Laws -- (15 test marks, worth 15 Exercise Marks) --------
 
@@ -137,7 +135,9 @@ law3 e = case e of
           (Sub x (Add y z)) -> Just(Sub(Sub x y) z)
           _                 -> Nothing
 
-law4 :: Expr -> Maybe Expr --Will need to write a helper function of some sorts to make sure x == z, y== q
-law4 e = case e of    
-          (Mul (Add x y) (Sub z q)) -> Just(Sub (Mul x z) (Mul y q)) --Note need x == z and y == q
-          _                         -> Nothing
+law4 :: Expr -> Maybe Expr 
+law4 (Mul (Add x y) (Sub z q)) 
+        | x == z && y == q = Just(Sub (Mul x z) (Mul y q)) 
+        | otherwise = Nothing
+law4 _ = Nothing
+         
