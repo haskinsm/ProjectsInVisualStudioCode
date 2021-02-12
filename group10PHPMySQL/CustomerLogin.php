ba8a7313c6bac18237ca862a7a5c1001adc6e03c
haskinsm <60662825+haskinsm@users.noreply.github.com>
2021-02-12 13:53:00 +0000

    Menu Bar Added and core pages created of Website

--- group10PHPMySQL/CustomerLogin.php
+++ group10PHPMySQL/CustomerLogin.php
@@ -1,34 +1,21 @@
 <!-- 
-    Purpose of Script: Home page of website
+    Purpose of Script: Customer Login page, including create account link
     Written by: Michael H
-     last updated: Michael 12/02/21
+    last updated: Michael 12/02/21
 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HomePage</title>
+    <title>Customer Login</title>
     <link rel="stylesheet" href="WebsiteStyle.css"> <!-- All CSS should be added to the WebsiteStyle.css file and then it will be imported here. If want a unique 
             style for something should be done in line like so: E.G:   <h1 style="color:blue;text-align:center;">  This is a heading </h1>       -->
-
 </head>
 <body>
     <h1> Dublin Party Hire </h1>
 
     <?php include 'UniversalMenuBar.php';?> <!-- Imports code for menu bar from another php file-->
-
-    <p> 
-        Welcome to Dublin Party Hire! We have party rental equipment for any event at competitive prices. <br>
-        The company can supply everything you would need for a party such as furniture, glassware,
-        crockery, cutlery, linens and also barbecues, gazebos, marquees etc. <br>
-    </p>
-
-    <h2> Learn about our business </h2>
-
-    <p> 
-        We are a new business that values customer satisfaction very highly and are quality focused. The owners of DPH are Conor and Cathy Kelly.
-    </p>
-
 </body>
 </html>
---
+++ group10PHPMySQL/UniversalMenuBar.php
@@ -0,0 +1,55 @@
+<!-- 
+    Purpose of Script: Universal Menu Bar to be used in every page
+    Written by: Michael H
+    last updated: Michael 12/02/21
+-->
+
+<!-- Was neccesary to have the below css in this file as makes reference to topnav class which is created in this file and is not in scope if css
+     were to be included in the websiteStyle css file -->
+
+<style>
+    /* 
+       Reference : https://www.w3schools.com/howto/howto_js_topnav.asp
+        Add a black background color to the top navigation 
+    */
+    .topnav {
+        background-color: #333;
+        overflow: hidden;
+    }
+    
+    /* Style the links inside the navigation bar */
+    .topnav a {
+        float: left;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+    
+    /* Change the color of links on hover */
+    .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+    
+    /* The below style is not used currently, as would reduce maintainability of site as I'd need to actually put the code at the top of every php file and 
+       define the active page, can see if client wants it*/
+    /* Add a color to the active/current link 
+    .topnav a.active {
+        background-color: #4CAF50;
+        color: white;
+    }
+    */
+    
+</style>
+
+<div class="topnav">
+  <!-- <a class="active" href="HomePage.php">Home</a>              This was commented out as having a diff colour for active tab reduces site maintability-->
+  <a href="HomePage.php">Home</a>
+  <a href="ProductsAndPricing.php"> Products & Pricing  </a>  
+  <a href="CustomerLogin.php"> Customer Login  </a> 
+  <a href="StaffLogin.php"> Staff Login </a>
+  <a href="ManagementLogin.php"> Manager Login </a>
+  <a href="ContactUs.php"> Contact us  </a> 
+</div>
